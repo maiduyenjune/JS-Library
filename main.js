8fb495f8378716a076b3e5c01a3a885377d5f7a8
maiduyenjune <146368907+maiduyenjune@users.noreply.github.com>
2024-03-21 23:57:14 -0400

    add parsley

--- main.js
+++ main.js
@@ -14,3 +14,35 @@ window.onload = () => {
 
     masonry.on('layoutComplete', () => console.log('Layout Complete'))
 };
+
+// // Parsley
+$(function () {
+    $('#demo-form').parsley().on('field:validated', function () {
+        var ok = $('.parsley-error').length === 0;
+        $('.bs-callout-info').toggleClass('hidden', !ok);
+        $('.bs-callout-warning').toggleClass('hidden', ok);
+    })
+        .on('form:submit', function () {
+            return false; // Don't submit form for this demo
+        });
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
